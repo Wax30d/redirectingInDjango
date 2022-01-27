2eb591af7228191ea6f65d2049ea18136d4d423f
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-27 19:27:56 +0500

    Added page-redirection to URL.

--- redirectingInDjango/urls.py
+++ redirectingInDjango/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wax30d/', index),
+    path('redirect/', redirectFunction),
+    path('ggg/', redirectFunction),
 ]
