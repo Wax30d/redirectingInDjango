92f21398b880191a57f60cd84f39fba32b23c077
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-27 19:22:04 +0500

    Added redirect function to view.

--- redirectingInDjango/views.py
+++ redirectingInDjango/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 
 def index(request):
     html = """ <h1> Django Baby </h1> Hello, you just configured you First URL"""
     return HttpResponse(html)
+
+
+def redirectFunction(request):
+    return redirect('/wax30d')
